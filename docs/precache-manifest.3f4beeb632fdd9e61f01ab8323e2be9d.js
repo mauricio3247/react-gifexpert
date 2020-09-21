20e949e384aee1224b477e9af5672acedc3f3b3e
Mauricio Oropeza <mauricio.3247@gmail.com>
2020-09-20 22:37:16 -0400

    build docs

---
+++ docs/precache-manifest.3f4beeb632fdd9e61f01ab8323e2be9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fab63f91a6ac4a233c4b5bfa20b7f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a851eaef6b22d5825f18",
+    "url": "/static/css/main.9c01dc89.chunk.css"
+  },
+  {
+    "revision": "74ca8e61dc64a30e2fec",
+    "url": "/static/js/2.e908e0c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e908e0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a851eaef6b22d5825f18",
+    "url": "/static/js/main.68b86994.chunk.js"
+  },
+  {
+    "revision": "6bdcec1c1c2e4f5d82f3",
+    "url": "/static/js/runtime-main.688e9337.js"
+  }
+]);
